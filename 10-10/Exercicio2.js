7e7b8ac3595dbd7a13e70b1a1bec749188754dbd
mirele06 <mirelemlinmm06@gmail.com>
2024-10-11 17:52:40 -0300

    Update Exercicio2.js

--- 10-10/Exercicio2.js
+++ 10-10/Exercicio2.js
@@ -8,3 +8,12 @@
 
 let numero = 0 
 
+if (numero === 0) {
+  console.log("O número é 0.")
+}else if (numero > 0) {
+  console.log("O número é positivo.)
+}else {
+  console.log("O número é negativo.)
+}
+
+
